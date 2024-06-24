d018a59cac579338022fe5f73bc27c2b3f056861
Montgomery Teofilus <montgomery2515@gmail.com>
2024-06-24 08:20:28 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+// git init
+// git add .
+// git commit -m 'first commit'
+// git remote add origin https://github.com/Temontel/UTCWebProg.git
+// git branch -M main
+// git push -u origin main
